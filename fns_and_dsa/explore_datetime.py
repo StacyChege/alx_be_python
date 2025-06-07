0376ff5413992bd6e37396293a849894e414f45b
Stacy Chege <stacychege23@gmail.com>
2025-06-07 11:56:39 +0300

    Fix: updated prompt to match checker requirements exactly

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -8,7 +8,7 @@ def display_current_datetime():
     return current_date
 
 def calculate_future_date(days_to_add):
-    days = int(input("Enter the number of days to add to the current dae: "))
+    days = int(input("Enter the number of days to add to the current date: "))
     current_date = datetime.now()
     future_date = current_date = timedelta(days=days)
     print("Future date:", future_date.strftime("%Y-%m-%d"))
