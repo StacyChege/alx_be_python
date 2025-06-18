466ecea05c93757f16b0edad7a0e0a047f247b9c
Stacy Chege <stacychege23@gmail.com>
2025-06-19 00:06:24 +0300

    Add Book class with methods for checkout and availability management

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,3 +1,22 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False
+
+    def check_out(self):
+        """Mark the book as checked out."""
+        self._is_checked_out = True
+
+    def return_book(self):
+        """Mark the book as returned (available)."""
+        self._is_checked_out = False
+
+    def is_available(self):
+        """Return True if the book is not checked out."""
+        return not self._is_checked_out
+    
+    
 class Library:
     def __init__(self):
         self._books = []
