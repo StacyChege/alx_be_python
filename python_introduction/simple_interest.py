bffdadfc0ddbd45fd1118f235d109fe2de02387f
Stacy Chege <stacychege23@gmail.com>
2026-03-06 17:16:52 +0300

    Calculating simple interest rate

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,11 @@
+# Calculating simple interest
+
+principal = 1000 # In dollars
+rate = 0.05 # 5% annual interest rate
+time = 3 # In years
+
+# Simple Interest formula: SI = (P * R * T) 
+
+simple_interest = (principal * rate * time)
+
+print("The simple interest is:", simple_interest)
